96e7844cf0434ee45b53f566523c3e9e16b2bdf8
Jereme <jeremejazz15@gmail.com>
2022-11-30 22:15:55 +0800

    update captions

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
-"use strict"
+"use strict";
 
-/** @type {{search: string; lucky?: boolean;}} */
+/** @type {{search: string; lucky?: boolean; name: string;}} */
 const query = window.location.search
   .substr(1)
   .split("&")
-  .map(keyValue => keyValue.split("="))
+  .map((keyValue) => keyValue.split("="))
   .map(([key, value]) => ({
     [decodeURIComponent(key)]: decodeURIComponent(
       value?.replaceAll("+", "%20")
     ),
   }))
-  .reduce((previous, current) => ({ ...previous, ...current }), {})
+  .reduce((previous, current) => ({ ...previous, ...current }), {});
 
 /** @type {HTMLInputElement} */
-let input
+let input;
 
 window.addEventListener("load", async () => {
-  input = document.getElementById("input")
-  input.value = ""
+  input = document.getElementById("input");
+  input.value = "";
 
-  setBrightness(JSON.parse(localStorage.getItem("dark") ?? "false"))
+  makeQuote();
+  setBrightness(JSON.parse(localStorage.getItem("dark") ?? "false"));
 
-  if (!query.search) return
+  if (query.name) {
+    document.getElementById("namedrop").innerText = ", " + query.name;
+  }
+  if (!query.search) return;
 
-  await setMessage("Step 1", "Type in your question")
-  const cursor = makeCursor()
-  await move(cursor, input)
-  input.focus()
-  await write()
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  input.blur()
+  await setMessage("Step 1", "i-type ang iyong tanong");
+  const cursor = makeCursor();
+  await move(cursor, input);
+  input.focus();
+  await write();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  input.blur();
 
-  await setMessage("Step 2", "Click on the search button")
+  await setMessage("Step 2", "i-click ang search button");
   const button = query.lucky
     ? document.getElementById("lucky")
-    : document.getElementById("search")
-  await move(cursor, button)
-  button.focus()
-  await new Promise(resolve => setTimeout(resolve, 1000))
+    : document.getElementById("search");
+  await move(cursor, button);
+  button.focus();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  await setMessage("Come on", "Was it really that hard?", "alert-success")
-  await new Promise(resolve => setTimeout(resolve, 3000))
+  await setMessage("Ayan tapos", "mahirap ba?", "alert-success");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
   window.location.href = `https://www.google.com/search?${
     query.lucky ? "btnI&" : ""
-  }q=${query.search}`
-})
+  }q=${query.search}`;
+});
 
 function makeCursor() {
-  const dark = JSON.parse(localStorage.getItem("dark") ?? "false")
+  const dark = JSON.parse(localStorage.getItem("dark") ?? "false");
 
-  const cursor = document.createElement("span")
-  cursor.className = `bi-cursor-fill text-${dark ? "light" : "dark"}`
-  cursor.id = "cursor"
-  document.body.appendChild(cursor)
-  return cursor
+  const cursor = document.createElement("span");
+  cursor.className = `bi-cursor-fill text-${dark ? "light" : "dark"}`;
+  cursor.id = "cursor";
+  document.body.appendChild(cursor);
+  return cursor;
 }
 
 /**
@@ -63,41 +67,43 @@ function makeCursor() {
  * @param {HTMLButtonElement} target
  */
 async function move(cursor, target) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const diffX =
       target.getBoundingClientRect().left +
       target.clientWidth / 2 -
-      cursor.getBoundingClientRect().left
+      cursor.getBoundingClientRect().left;
     const diffY =
       target.getBoundingClientRect().top +
       target.clientHeight / 2 -
-      cursor.getBoundingClientRect().top
+      cursor.getBoundingClientRect().top;
 
-    const steps = 60
-    const stepX = diffX / steps
-    const stepY = diffY / steps
+    const steps = 60;
+    const stepX = diffX / steps;
+    const stepY = diffY / steps;
 
-    let step = 0
-    const interval = setInterval(frame, 1000 / 60)
+    let step = 0;
+    const interval = setInterval(frame, 1000 / 60);
 
     function frame() {
       if (step >= steps) {
-        clearInterval(interval)
-        resolve()
+        clearInterval(interval);
+        resolve();
       } else {
-        step++
-        cursor.style.top = (parseFloat(cursor.style.top) || 0) + stepY + "px"
-        cursor.style.left = (parseFloat(cursor.style.left) || 0) + stepX + "px"
+        step++;
+        cursor.style.top = (parseFloat(cursor.style.top) || 0) + stepY + "px";
+        cursor.style.left = (parseFloat(cursor.style.left) || 0) + stepX + "px";
       }
     }
-  })
+  });
 }
 
 async function write() {
   for (const letter of query.search) {
-    await new Promise(resolve => setTimeout(resolve, Math.random() * 200 + 100))
-    input.value += letter
-    input.scrollLeft = input.scrollWidth
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.random() * 200 + 100)
+    );
+    input.value += letter;
+    input.scrollLeft = input.scrollWidth;
   }
 }
 
@@ -108,25 +114,25 @@ async function write() {
  * @param {string} type
  */
 async function setMessage(heading, content, type = "alert-primary") {
-  const message = document.getElementById("message")
+  const message = document.getElementById("message");
 
-  message.classList.add("opacity-0")
-  await new Promise(resolve => setTimeout(resolve, 300))
+  message.classList.add("opacity-0");
+  await new Promise((resolve) => setTimeout(resolve, 300));
 
-  message.classList.remove("alert-primary")
-  message.classList.remove("alert-success")
-  message.classList.add(type)
-  document.getElementById("message-heading").innerText = heading
-  document.getElementById("message-content").innerText = content
+  message.classList.remove("alert-primary");
+  message.classList.remove("alert-success");
+  message.classList.add(type);
+  document.getElementById("message-heading").innerText = heading;
+  document.getElementById("message-content").innerText = content;
 
-  message.classList.remove("opacity-0")
-  await new Promise(resolve => setTimeout(resolve, 300))
+  message.classList.remove("opacity-0");
+  await new Promise((resolve) => setTimeout(resolve, 300));
 }
 
 function toggleBrightness() {
-  const dark = JSON.parse(localStorage.getItem("dark") ?? "false")
-  localStorage.setItem("dark", !dark)
-  setBrightness(!dark)
+  const dark = JSON.parse(localStorage.getItem("dark") ?? "false");
+  localStorage.setItem("dark", !dark);
+  setBrightness(!dark);
 }
 
 /**
@@ -134,8 +140,8 @@ function toggleBrightness() {
  * @param {boolean} dark
  */
 function setBrightness(dark) {
-  const newbrightness = dark ? "dark" : "light"
-  const oldBrightness = dark ? "light" : "dark"
+  const newbrightness = dark ? "dark" : "light";
+  const oldBrightness = dark ? "light" : "dark";
 
   for (const oldClass of [
     `bg-${oldBrightness}`,
@@ -143,18 +149,43 @@ function setBrightness(dark) {
     `btn-${oldBrightness}`,
     `border-${oldBrightness}`,
   ]) {
-    const newClass = oldClass.replace(oldBrightness, newbrightness)
+    const newClass = oldClass.replace(oldBrightness, newbrightness);
     for (const element of document.querySelectorAll(`.${oldClass}`)) {
-      element.classList.remove(oldClass)
-      element.classList.add(newClass)
+      element.classList.remove(oldClass);
+      element.classList.add(newClass);
     }
   }
 
   for (const oldClass of [`text-${newbrightness}`]) {
-    const newClass = oldClass.replace(newbrightness, oldBrightness)
+    const newClass = oldClass.replace(newbrightness, oldBrightness);
     for (const element of document.querySelectorAll(`.${oldClass}`)) {
-      element.classList.remove(oldClass)
-      element.classList.add(newClass)
+      element.classList.remove(oldClass);
+      element.classList.add(newClass);
     }
   }
+}
+
+function makeQuote() {
+  var igmQuotes = [
+    "Ako nalang mag Google para sayo.",
+    "Hanap hanap din pag may time.",
+    "Itanong mo kay Kuya Google.",
+    "Hindi porket naka FREE FACEBOOK ka, sa FACEBOOK ka na magtatanong.",
+    "Bawas bawas din ng pag tatanong pag may time...",
+    "If you're not willing to learn, no one can help you.",
+    "Pwede naman kasing i-search. Ba't magtatanong pa?",
+    "Torpe ka ba? Search mo 'Torpe Tips'",
+    "Talo ba kayo? Search mo 'ML Tips'",
+    "Magtatanong ka pa lang, nahanap ko na.",
+    "Kamusta naman ang pag hahanap?",
+    "Wala kang mahanap? Search mo 'Do a Barrel Roll'",
+    "Wag mahiyang magtanong...",
+    "Maraming libreng kaalaman dito.",
+    "Kinabukasan ng ating bayan, ay siguradong makakamtan.",
+    "Yung katanungan mo malamang may nagtanong na rin nyan dati.",
+  ];
+
+  var quote = igmQuotes[Math.round(Math.random() * (igmQuotes.length - 1))];
+
+  document.getElementById("quote").innerText = quote;
 }
